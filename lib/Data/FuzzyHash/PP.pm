36cfac9d7d8b60b2a6e69f85d01764d50f8ba23d
hideo55 <hide.o.j55@gmail.com>
2013-05-16 23:37:58 +0900

    Start development Data::FuzzyHash::PP

---
+++ lib/Data/FuzzyHash/PP.pm
@@ -0,0 +1,19 @@
+package Data::FuzzyHash::PP;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+sub fuzzy_hash {
+    
+}
+
+sub fuzzy_hash_file {
+    
+}
+
+sub fuzzy_compare {
+    
+}
+
+1;
+__END__
